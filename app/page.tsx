3b4eaceb8aa0ce3704b42b00b7eb8049ce859f30
massacc <massacc@protonmail.com>
2024-06-06 23:54:58 +0200

    c1

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+export default function Home() {
+  return (
+    <div>
+      <Link href="/about">About</Link>
+      <p>Bernard Opieliński</p>
+      <p>numer indeksu: 119247</p>
+    </div>
+  );
+}
